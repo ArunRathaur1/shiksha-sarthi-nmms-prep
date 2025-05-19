e59bb8a9886850d49742ff7af129c3517b4889b7
ArunRathaur1 <arunrathaur92.6@gmail.com>
2025-05-19 17:31:39 +0530

    updated the code

--- backend/routes/question.js
+++ backend/routes/question.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Question = require("../models/Question");
-
-const GEMINI_API_KEY = "AIzaSyDAz3hV4RNZJitFUFY_3RGbuPE4FCw49bY";
+require("dotenv").config();
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 router.post("/", async (req, res) => {
