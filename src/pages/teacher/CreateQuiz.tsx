349899d367208aa339f873b4c06f56c026edaa20
bhaskar601 <bhaskarsahu2605@gmail.com>
2025-05-24 11:17:45 +0530

    updated the code

--- src/pages/teacher/CreateQuiz.tsx
+++ src/pages/teacher/CreateQuiz.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Cookies from "js-cookie";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
 
 interface Question {
   _id: string;
